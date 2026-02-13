d8b19b7c55bd0bc9c564ee6f811858b1a8261a61
garvitkumar854 <garvitkajot854@gmail.com>
2026-02-13 19:20:31 +0530

    Improvements

--- Assignment49.java
+++ Assignment49.java
@@ -1,15 +1,16 @@
 // Demonstrate the use of Thread class by creating a thread and printing the name of the thread and a message
 class Assignment49 extends Thread{
-    public static void main(String[] args){
+
+    public void run(){
         System.out.println(Thread.currentThread().getName());
         for(int i=0; i<3; i++){
             System.out.println("Count " + i);
         }
     }
     public static void main(String[] args){
-        System.out.println(Thread.currentThread().getName());
+        System.out.print(Thread.currentThread().getName());
         System.out.println("Thread");
-        Assignment obj = new Assignment();
+        Assignment49 obj = new Assignment49();
         obj.start();
     }
 }
